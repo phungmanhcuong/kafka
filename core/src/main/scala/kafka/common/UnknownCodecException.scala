e587617940c0773f5b344193e3ce3d80a0bcec2d
Phung Manh Cuong <phung.cuong@thoughtworks.com>
2021-05-16 09:47:24 +0800

    Revert previous success build

--- core/src/main/scala/kafka/common/UnknownCodecException.scala
+++ core/src/main/scala/kafka/common/UnknownCodecException.scala
@@ -1,11 +1,11 @@
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
+ * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
+ * the License.  You may obtain a copy of the License at
+ * 
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -14,17 +14,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package kafka.common;
 
-import scala.Product;
+package kafka.common
 
-import java.io.Serializable;
-
-/*
- * We inherit from `Product` and `Serializable` because `case` objects and classes inherit from them and if we don't
- * do it here, the compiler will infer types that unexpectedly include `Product` and `Serializable`, see
- * http://underscore.io/blog/posts/2015/06/04/more-on-sealed.html for more information.
+/**
+ * Indicates the client has requested a range no longer available on the server
  */
-public interface BaseEnum extends Product, Serializable {
-    String getName();
+class UnknownCodecException(message: String) extends RuntimeException(message) {
+  def this() = this(null)
 }
+
