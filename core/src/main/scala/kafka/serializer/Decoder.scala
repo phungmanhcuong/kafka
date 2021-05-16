79e2d0cb57f558f935684c81ea79001fc63b2b95
Phung Manh Cuong <phung.cuong@thoughtworks.com>
2021-05-16 23:01:56 +0800

    Revert previous change

--- core/src/main/scala/kafka/serializer/Decoder.scala
+++ core/src/main/scala/kafka/serializer/Decoder.scala
@@ -18,13 +18,22 @@
 package kafka.serializer
 
 import java.nio.ByteBuffer
+
 import kafka.utils.VerifiableProperties
-import org.apache.kafka.reusable.serializer.Decoder
+
+/**
+ * A decoder is a method of turning byte arrays into objects.
+ * An implementation is required to provide a constructor that
+ * takes a VerifiableProperties instance.
+ */
+trait Decoder[T] {
+  def fromBytes(bytes: Array[Byte]): T
+}
 
 /**
  * The default implementation does nothing, just returns the same byte array it takes in.
  */
-class DefaultDecoder(props: VerifiableProperties = null) extends Decoder[Array[Byte], Array[Byte]] {
+class DefaultDecoder(props: VerifiableProperties = null) extends Decoder[Array[Byte]] {
   def fromBytes(bytes: Array[Byte]): Array[Byte] = bytes
 }
 
@@ -32,7 +41,7 @@ class DefaultDecoder(props: VerifiableProperties = null) extends Decoder[Array[B
  * The string decoder translates bytes into strings. It uses UTF8 by default but takes
  * an optional property serializer.encoding to control this.
  */
-class StringDecoder(props: VerifiableProperties = null) extends Decoder[Array[Byte], String] {
+class StringDecoder(props: VerifiableProperties = null) extends Decoder[String] {
   val encoding =
     if(props == null)
       "UTF8"
@@ -47,7 +56,7 @@ class StringDecoder(props: VerifiableProperties = null) extends Decoder[Array[By
 /**
   * The long decoder translates bytes into longs.
   */
-class LongDecoder(props: VerifiableProperties = null) extends Decoder[Array[Byte], Long] {
+class LongDecoder(props: VerifiableProperties = null) extends Decoder[Long] {
   def fromBytes(bytes: Array[Byte]): Long = {
     ByteBuffer.wrap(bytes).getLong
   }
@@ -56,7 +65,7 @@ class LongDecoder(props: VerifiableProperties = null) extends Decoder[Array[Byte
 /**
   * The integer decoder translates bytes into integers.
   */
-class IntegerDecoder(props: VerifiableProperties = null) extends Decoder[Array[Byte], Integer] {
+class IntegerDecoder(props: VerifiableProperties = null) extends Decoder[Integer] {
   def fromBytes(bytes: Array[Byte]): Integer = {
     ByteBuffer.wrap(bytes).getInt()
   }
--- reusable/src/main/java/org/apache/kafka/reusable/serializer/Decoder.java
+++ reusable/src/main/java/org/apache/kafka/reusable/serializer/Decoder.java
@@ -23,5 +23,5 @@ package org.apache.kafka.reusable.serializer;
  * takes a VerifiableProperties instance.
  */
 public interface Decoder<In, Out> {
-    Out fromBytes(In in);
+    Out decode(In in);
 }
